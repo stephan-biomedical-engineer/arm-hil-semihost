c1af835834ed110e58a2db5fd72b869308899f48
stephan-biomedical-engineer <stephanbrrs8@gmail.com>
2026-04-08 00:08:16 -0300

    Makefile flags correction

--- hil_api/hil.mk
+++ hil_api/hil.mk
@@ -1,14 +1,22 @@
-ifeq ($(ENABLE_HIL), 1)
-    $(info >>> [HIL API] MODO HIL ATIVADO: Injetando Semihosting <<<)
+# hil_framework/hil_api/hil.mk
 
-    # Remove syscalls.o e sysmem.o da lista de objetos
-    # O Makefile da ST usa $(C_SOURCES:.c=.o) para gerar $(OBJECTS).
+ifeq ($(ENABLE_HIL), 1)
+    $(info >>> [HIL API] MODO HIL ATIVADO: Ajustando variáveis de build <<<)
+
     C_SOURCES := $(filter-out %/syscalls.c %/sysmem.c, $(C_SOURCES))
 
     C_SOURCES += hil_framework/hil_api/src/hil_test.c
     C_INCLUDES += -Ihil_framework/hil_api/inc
+    
+    OBJECTS += $(BUILD_DIR)/hil_test.o
+    
+    vpath hil_test.c hil_framework/hil_api/src
 
     C_DEFS += -DUSE_SEMIHOSTING -DHIL_ACTIVE
+
+    LIBS := $(filter-out -lnosys, $(LIBS))
     LIBS += -lrdimon
+
+    LDFLAGS := $(filter-out --specs=nano.specs, $(LDFLAGS))
     LDFLAGS += --specs=rdimon.specs
 endif
