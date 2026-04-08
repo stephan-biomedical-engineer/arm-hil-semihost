9e33322bd1b3388a64d4910bc91cc24b0e4a31f9
stephan-biomedical-engineer <stephanbrrs8@gmail.com>
2026-04-08 00:14:37 -0300

    FIX: HIL Makefile injection objects to STM32 Makefile

--- hil_api/hil.mk
+++ hil_api/hil.mk
@@ -1,22 +1,22 @@
 # hil_framework/hil_api/hil.mk
 
 ifeq ($(ENABLE_HIL), 1)
-    $(info >>> [HIL API] MODO HIL ATIVADO: Ajustando variáveis de build <<<)
+    $(info >>> [HIL API] MODO HIL ATIVADO: Injeção de dependências <<<)
 
     C_SOURCES := $(filter-out %/syscalls.c %/sysmem.c, $(C_SOURCES))
-
-    C_SOURCES += hil_framework/hil_api/src/hil_test.c
-    C_INCLUDES += -Ihil_framework/hil_api/inc
-    
-    OBJECTS += $(BUILD_DIR)/hil_test.o
-    
-    vpath hil_test.c hil_framework/hil_api/src
+    LIBS := $(filter-out -lnosys, $(LIBS))
+    LDFLAGS := $(filter-out -specs=nano.specs, $(LDFLAGS))
 
     C_DEFS += -DUSE_SEMIHOSTING -DHIL_ACTIVE
-
-    LIBS := $(filter-out -lnosys, $(LIBS))
+    C_INCLUDES += -Ihil_framework/hil_api/inc
+    
     LIBS += -lrdimon
-
-    LDFLAGS := $(filter-out --specs=nano.specs, $(LDFLAGS))
     LDFLAGS += --specs=rdimon.specs
+
+    HIL_OBJ = $(BUILD_DIR)/hil_test.o
+    OBJECTS := $(filter-out $(HIL_OBJ), $(OBJECTS)) $(HIL_OBJ)
+
+    $(BUILD_DIR)/hil_test.o: hil_framework/hil_api/src/hil_test.c Makefile | $(BUILD_DIR)
+	$(CC) -c $(CFLAGS) -Wa,-a,-ad,-alms=$(BUILD_DIR)/hil_test.lst $< -o $@
+
 endif
